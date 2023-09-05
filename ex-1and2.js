39e4248bf509ba35a1a8d1dfc35def8a981fdc7d
GaiKT <124340777+GaiKT@users.noreply.github.com>
2023-09-05 16:40:28 +0700

    fix(ex-1,2,3,4): Completed assignment value&variable

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,17 @@
 //Exercise #1: Variable Creation
 //Start coding here
 
+let fristName = "Karter"
+let lastName = "Anderson"
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai"
+
+const restaurantName = "Chef's Table by Chef James"
+const price = "15000"
+const countRestaurantActive = "1500"
+
 //Exercise #2: Variable Reassignment
 //Start coding here
+
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641"
+price = "5000" //Variable ที่ Declear ด้วย const ไม่สามารถ Reassign ได้
+countRestaurantActive = "2500" //Variable ที่ Declear ด้วย const ไม่สามารถ Reassign ได้
