134aaa9f9eb8e437b35485abda23cb62e5461df3
RafahCTR <rafahphanthila@gmail.com>
2023-10-01 18:56:53 +0700

    Assignment: Values Variables

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,10 @@
 //Exercise #1: Variable Creation
-//Start coding here
+let name = "Karter";
+let lastname = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
 
 //Exercise #2: Variable Reassignment
-//Start coding here
+const restaurent = "Chef's Table by Chef James";
+const price = 15000;
+const NumberRestaurent = 1500;
+
